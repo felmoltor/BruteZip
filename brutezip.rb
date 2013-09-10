2defc94694c38172d7cd0604c1d777eb4e3db5af
Felipe Molina <felmoltor@gmail.com>
2013-09-10 11:52:12 +0200

    If the number of threads is only 1. We don't need to copy the whole dictionary file to another.

--- brutezip.rb
+++ brutezip.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require './include/BruteZip.rb'
 require 'optparse'
 require 'pp'
+require 'curses'
 
 def getArguments
   # This hash will hold all of the options
@@ -63,6 +64,27 @@ end
 
 # ===================
 
+def captureKeystrokesCommands
+  # TODO: Create a thread that will capture keystrokes to show completed percentage
+
+  Curses.noecho # do not show typed keys
+  Curses.init_screen
+  Curses.stdscr.keypad(true) # enable arrow keys (required for pageup/down)
+  
+  loop do
+    case Curses.getch
+    when Curses::Key::ENTER 
+      Curses.setpos(0,0)
+      Curses.addstr("Showing percentage of process")
+    when Curses::Key::EXIT
+      Curses.setpos(0,0)
+      Curses.addstr("Exiting application. Please wait...")
+    end
+  end
+end
+
+# ===================
+
 def calculateDictionaryOffsets(dictionary,nparts)
   dictionarysize = %x{wc -l '#{dictionary}'}.split.first.to_i
   last_offset = 0
@@ -97,31 +119,35 @@ def distributeDictionaryFile(dictionary,nthreads)
   subdict = nil
   ndict = 0
   
-  # Create N files with the subdictionaries
-  for offset in offsets
-    dict = File.open(dictionary,"r")
-    nline = 0
-    ndict += 1
-    
-    dict.each { |line|
-      if nline == offset[0]
-        subdict = File.new("#{File.dirname(dictionary)}#{File::SEPARATOR}.#{File.basename(dictionary)}.#{ndict}","w")
-        subdictionaries << subdict.path
-      end
+  if nthreads > 1
+    # Create N files with the subdictionaries
+    for offset in offsets
+      dict = File.open(dictionary,"r")
+      nline = 0
+      ndict += 1
       
-      if nline > offset[0] and nline < offset[1]
-        subdict.puts line.chomp
-      end
-      
-      # If is the last line of the subdictionary
-      if nline == offset[1] - 1
-        subdict.close
-        break
-      end
-      nline += 1
-    }
-    dict.close
-  end # Del for offset in offsets
+      dict.each { |line|
+        if nline == offset[0]
+          subdict = File.new("#{File.dirname(dictionary)}#{File::SEPARATOR}.#{File.basename(dictionary)}.#{ndict}","w")
+          subdictionaries << subdict.path
+        end
+        
+        if nline > offset[0] and nline < offset[1]
+          subdict.puts line.chomp
+        end
+        
+        # If is the last line of the subdictionary
+        if nline == offset[1] - 1
+          subdict.close
+          break
+        end
+        nline += 1
+      }
+      dict.close
+    end # Del for offset in offsets
+  else
+    subdictionaries << dictionary
+  end
   
   return subdictionaries
 end
@@ -144,9 +170,14 @@ password_found = "<NOT_FOUND>"
 opts = getArguments
 printBanner
 
+# TODO: curses thread is blocking the rest of the program
+# captureKeystrokesCommands
+
 # Split in N threads when brute forcing
 # Separate the dictionary in N sets and distribute to each thread
 puts "Splitting the dicctionary to the threads..."
+# TODO: Optimice this dictionary distribution. Takes too much time to split the dictionary
+#       Instead of creating N files, make the threads read the position of the original dictionary file
 subdicts = distributeDictionaryFile(opts[:dictionary],opts[:nthreads])
 
 # TODO: Create static method to check if is password protected
